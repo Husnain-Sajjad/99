5db385fdaaf00ead38ca99a4346035330bd95661
Sebastian <seb.doyon21@gmail.com>
2022-09-24 10:58:57 -0400

    commit

--- lib/add.dart
+++ lib/add.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:ntp/ntp.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firestore_methods.dart';
 
@@ -11,6 +15,12 @@ class AddPost extends StatefulWidget {
   @override
   State<AddPost> createState() => _AddPostState();
 }
+
+String nationalPost = '0';
+Timer? timer;
+var durationForMinutes = 0;
+var durationForHours = 0;
+DateTime ntpTime = DateTime.now();
 
 class _AddPostState extends State<AddPost> {
   final TextEditingController _titleController = TextEditingController();
@@ -33,10 +43,62 @@ class _AddPostState extends State<AddPost> {
     }
   }
 
+  getNationalPost() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.getString('nationalPost') != null) {
+      setState(() {
+        nationalPost = prefs.getString('nationalPost')!;
+      });
+    }
+  }
+
+  setNationalPost(String nationalPostValue) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      nationalPost = nationalPostValue.toString();
+      prefs.setString('nationalPost', nationalPost);
+    });
+  }
+
+  _startTimer() async {
+    timer = Timer.periodic(const Duration(minutes: 1), (Timer t) async {
+      var ntpTime = await NTP.now(lookUpAddress: '1.amazon.pool.ntp.org');
+      // var dateNow = DateTime.now();
+      setState(() {
+        durationForMinutes = 59 - ntpTime.minute;
+        durationForHours = 23 - ntpTime.hour;
+      });
+      durationForMinutes == 13 && durationForHours == 13
+          ? setNationalPost('0')
+          : null;
+    });
+  }
+
+  _initTimer() async {
+    var ntpTime = await NTP.now(lookUpAddress: '1.amazon.pool.ntp.org');
+    // if (!initialized)
+    setState(() {
+      durationForMinutes = 59 - ntpTime.minute;
+      durationForHours = 23 - ntpTime.hour;
+    });
+    durationForMinutes == 13 && durationForHours == 13
+        ? setNationalPost('0')
+        : null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initTimer();
+    _startTimer();
+    getNationalPost();
+  }
+
   @override
   void dispose() {
     super.dispose();
     _titleController.dispose();
+    timer?.cancel();
   }
 
   @override
@@ -56,6 +118,7 @@ class _AddPostState extends State<AddPost> {
                   : InkWell(
                       onTap: () {
                         sendPost();
+                        setNationalPost('1');
                       },
                       child: Container(
                         width: 200,
@@ -71,6 +134,9 @@ class _AddPostState extends State<AddPost> {
                       ),
                     ),
             ),
+            Text('minutes: ${durationForMinutes}'),
+            Text('hours: ${durationForHours}'),
+            Text('nationalPost: ${nationalPost}')
           ],
         ),
       ),
